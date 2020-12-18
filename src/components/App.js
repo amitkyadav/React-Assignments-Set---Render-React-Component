06cee6d58f2cf0e82d64f7da8928299910855b91
amitkyadav <38914912+amitkyadav@users.noreply.github.com>
2020-12-18 10:51:05 +0530

    p tag rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
-
-
+  return <p>I am learning React. My life is getting better.</p>;
+};
 export default App;
